7520aacd495ff8cd06bf95f1dc61dac40dd02c47
FaimaliG <85684007+Theimx@users.noreply.github.com>
2024-11-14 21:42:31 +0100

    Add files via upload

---
+++ exo1.py
@@ -0,0 +1,9 @@
+
+
+def moyenne_math2(note1,note2,note3,note4,note5):
+    _math = (((note1+note2+note3) / 3) + ((note4+note4+note5+note5)) / 4) /2
+
+    return _math
+
+print(moyenne_math2(20,20,20,0,0))
+
