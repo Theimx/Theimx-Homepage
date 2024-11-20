a3a192f0fb071947ff1417a369da587bf170bd6b
FaimaliG <85684007+Theimx@users.noreply.github.com>
2024-11-20 22:06:20 +0100

    Update exo2.py

--- PYTHON_NSI/exo2.py
+++ PYTHON_NSI/exo2.py
@@ -1,24 +1,22 @@
-
+enceinte = 0
 age = int(input("entrez votre age: "))
-sexe = input("si vous êtes une femme ecrivez oui : ")
-enceinte = int(input("si vous êtes enceinte entrez oui: "))
-sexe_femme = 0
 
-
-if sexe == oui:
+sexe_femme = input("si vous êtes une femme entrez : oui : ")
+if sexe_femme == "oui":
     sexe_femme = True
-elif sexe != oui:
-    sexe_femme = False
-    
+
+if sexe_femme == True:
+    enceinte = input("si vous êtes enceinte écrivez : oui: ")
+if enceinte == "oui":
+    enceinte = True
 
 
-
-
-if age >= 17.99 : 
+if age <= 17.99 : 
     print("non trop petit")
 
-if sexe_femme == True and enceinte == oui:
+if sexe_femme == True and enceinte == True:
     print("non pas bon pour le bébé")
-else :
-    print("oui")
+if age >= 18 and enceinte != "oui" :
+    print("oui c'est bon")
+    
     
