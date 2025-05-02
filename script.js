4ee4516bf5d1a4aeff3e6a719deeaf5f10db8692
Theimx <85684007+Theimx@users.noreply.github.com>
2025-05-02 11:42:37 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const sidebar = document.getElementById('sidebar');
-const content = document.getElementById('content');
-const toggleBtn = document.getElementById('toggle-btn');
-
-toggleBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('collapsed');
-    content.classList.toggle('collapsed');
-});
